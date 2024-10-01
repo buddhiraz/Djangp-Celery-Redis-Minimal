bc6a0a40df6a274b40b6c15b4de9ecdbfe175088
buddhiraz <buddhirajsahu@gmail.com>
2024-10-01 18:04:36 +0530

    yml fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,22 +6,40 @@ resource "aws_ecs_cluster" "ecs_cluster" {
   name = "360health-backend"
 }
 
-resource "aws_ecs_service" "ecs_service" {
-  name            = "360health-backend-service"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.django_task_def.arn
-  desired_count   = 1
-  launch_type     = "EC2"
-}
-
-
-
 resource "aws_ecs_task_definition" "django_task_def" {
   family                   = "django-task"
-  container_definitions    = file("../infra/ecs-task-definition.json")  # Use correct relative path
+  container_definitions    = file("../infra/ecs-task-definition.json")
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   memory                   = "512"
   cpu                      = "256"
+}
+
+resource "aws_ecs_service" "ecs_service" {
+  name            = "360health-backend-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.django_task_def.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"  # Change this if using EC2
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs_task_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
+}
+
+resource "aws_iam_policy_attachment" "ecs_task_execution_role_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  roles      = [aws_iam_role.ecs_task_execution_role.name]
+}
+
+data "aws_iam_policy_document" "ecs_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
 }
