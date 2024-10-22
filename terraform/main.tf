8bb8fd091ed566eebfb6f1808d3bd0d19a0c0e77
BuddhirajSahu <47522091+buddhiraz@users.noreply.github.com>
2024-10-22 16:04:33 +0530

    Update main.tf for Cloud Log

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,6 +113,16 @@ resource "aws_security_group" "ecs_service_sg" {
   }
 }
 
+# Create CloudWatch Log Group
+resource "aws_cloudwatch_log_group" "ecs_log_group" {
+  name              = "/ecs/${var.ecs_task_family}"
+  retention_in_days = 7
+
+  tags = {
+    Name = "/ecs/${var.ecs_task_family}"
+  }
+}
+
 # ECS Task Definition
 resource "aws_ecs_task_definition" "task_definition" {
   family                   = var.ecs_task_family
@@ -128,6 +138,8 @@ resource "aws_ecs_task_definition" "task_definition" {
     container_port = var.container_port
     region         = var.region
   })
+
+  depends_on = [aws_cloudwatch_log_group.ecs_log_group]
 }
 
 # ECS Service
