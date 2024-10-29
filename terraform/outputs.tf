5b5caf7b59d7e5f8fd288712603fc335610af351
buddhiraz <buddhirajsahu@gmail.com>
2024-10-29 22:15:39 +0530

    Minor fixes in TF files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,15 +9,3 @@ output "ecs_service_name" {
   description = "Name of the ECS Service"
   value       = aws_ecs_service.ecs_service.name
 }
-
-# Fetch Public IP of ECS Task
-data "aws_ecs_task" "task" {
-  cluster = aws_ecs_cluster.ecs_cluster.id
-  task_arn = aws_ecs_service.ecs_service.task_arns[0]
-  depends_on = [aws_ecs_service.ecs_service]
-}
-
-output "ecs_task_public_ip" {
-  description = "Public IP address of the ECS task"
-  value       = data.aws_ecs_task.task.network_interfaces[0].association.public_ip
-}
