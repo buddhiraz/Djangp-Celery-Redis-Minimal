d1ec632bf318d414f43c7773afe7504013d33150
buddhiraz <buddhirajsahu@gmail.com>
2024-10-29 21:21:49 +0530

    CORS Allowed  for all

--- celery_example/settings.py
+++ celery_example/settings.py
@@ -47,9 +47,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # 3rd Party App
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -58,6 +62,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "celery_example.urls"
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-if [ "$1" = "celery" ]; then
-    # Run Celery worker
-    celery -A celery_example worker --loglevel=info
-elif [ "$1" = "celery-beat" ]; then
-    # Run Celery beat
-    celery -A celery_example beat --loglevel=info
-else
-    # Run Django development server
-    python manage.py migrate
-    python manage.py runserver 0.0.0.0:8000
-fi
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Wait for Redis to be available
+echo "Waiting for Redis to be available..."
+until nc -z redis 6379; do
+    echo "Redis is unavailable - sleeping"
+    sleep 1
+done
+echo "Redis is up - continuing"
+
+# Run Django migrations
+echo "Running migrations..."
+python manage.py migrate
+
+# Start the Django development server
+echo "Starting Django server..."
+python manage.py runserver 0.0.0.0:8000
